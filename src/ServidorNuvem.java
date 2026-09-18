1ee2c691dc600bd37052d93a255ebc0791a5668b
Bruno Cesar Kimura <bkimura@ciandt.com>
2026-09-18 00:05:04 -0300

    feat: server and thread initial setup

--- src/ServidorNuvem.java
+++ src/ServidorNuvem.java
@@ -1,2 +1,33 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServidorNuvem {
+    public static void main(String[] args) {
+        try {
+            ServerSocket welcomeSocket = new ServerSocket(6789);
+            while (true) {
+                Socket connectionSocket = welcomeSocket.accept();
+                ThreadServidor threadServidor = new ThreadServidor(connectionSocket);
+                Thread thread = new Thread(threadServidor);
+                thread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+class ThreadServidor implements Runnable{
+    private Socket socket;
+    ThreadServidor(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
 }
