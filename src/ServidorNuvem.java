4ec2fdc8cc53d86a92bf416e0875cd2c10c7de73
Bruno Cesar Kimura <bkimura@ciandt.com>
2026-09-18 00:32:48 -0300

    feat: adding some logs and testing stuff for server

--- src/ServidorNuvem.java
+++ src/ServidorNuvem.java
@@ -1,33 +1,47 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServidorNuvem {
+    private static final int PORTA = 6789;
+
     public static void main(String[] args) {
         try {
-            ServerSocket welcomeSocket = new ServerSocket(6789);
+            ServerSocket welcomeSocket = new ServerSocket(PORTA);
+            System.out.println("Servidor rodando na porta " + PORTA);
             while (true) {
                 Socket connectionSocket = welcomeSocket.accept();
-                ThreadServidor threadServidor = new ThreadServidor(connectionSocket);
-                Thread thread = new Thread(threadServidor);
+                System.out.println("Cliente conectado.");
+                Thread thread = new Thread(new ThreadServidor(connectionSocket));
                 thread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
 
 class ThreadServidor implements Runnable{
     private Socket socket;
+
     ThreadServidor(Socket socket){
         this.socket = socket;
     }
 
     @Override
     public void run() {
-
+        while(true){
+            try {
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                while(true){
+                    String mensagem = inFromClient.readLine();
+                    System.out.println(mensagem);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
-
 }
